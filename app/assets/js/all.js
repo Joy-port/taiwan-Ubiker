6836c274dcf26a1b2eb556d929df8f07281a7fc1
Joy Cheng <joy.cheng97324@gmail.com>
2021-11-22 00:39:47 +0800

    edit

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -2115,7 +2115,7 @@ expendBtn.addEventListener('click' , changeExpendStatus);
 function changeExpendStatus(e){
 const btnContent = document.querySelector('.js-expend-btn span')
   if(e.target.nodeName ==="SPAN" || e.target.nodeName === "BUTTON"){
-    expendBox.classList.toggle('top-75');
+    expendBox.classList.toggle('top-80');
     expendBox.classList.toggle('top-50');
 
     if(expendBox.classList.contains('top-50')){
